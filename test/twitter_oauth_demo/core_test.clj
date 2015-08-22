6f4843a087579f0418fbf9021d18abaf2aaa717c
Oleh Palianytsia <oleh.palianytsia@gmail.com>
2015-08-22 18:07:06 +0300

    Initial commit

---
+++ test/twitter_oauth_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns twitter-oauth-demo.core-test
+  (:require [clojure.test :refer :all]
+            [twitter-oauth-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
